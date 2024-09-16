e80298f1be7570e7536e3cf7158778578d3addd5
Samit26 <samitkhedekar2594@gmail.com>
2024-09-16 21:01:46 +0530

    commit2

--- script.js
+++ script.js
@@ -41,3 +41,24 @@ window.addEventListener("scroll", () => {
     }
   });
 });
+
+let a = 1;
+setInterval(() => {
+  const text1 = document.getElementById("text-12");
+  const text2 = document.getElementById("text-13");
+  if (a == 1) {
+    text1.innerText = "Navarro reduces time to hire by 50%";
+    text2.innerText = ` "We’ve been filling positions a lot faster because our managers are
+            now involved in the hiring process. So far we’ve made 150 hires in 6
+            months and we’ve reduced our time to hire from 50 days to 26."`;
+    a = 2;
+  } else if (a == 2) {
+    text1.innerText = "JOEY Restaurants cuts agency spend by 75%";
+    text2.innerText = `"With the insights we can gather using Workable, we’re able to reduce our dependence on external recruitment agencies to just one or two specialty roles a year."`;
+    a = 3;
+  } else if (a == 3) {
+    text1.innerText = "Eurobank increases apply rate by 22%";
+    text2.innerText = `"By using Workable, we’re now attracting a new generation of candidates and building a richer and more robust workforce."`;
+    a = 1;
+  }
+}, 1500);
